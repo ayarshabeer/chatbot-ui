cb3ddbbdd1f0bdddcc1b6864eccd1e5ec8881080
Shabeer Ayar <ayarshabeer@192.168.1.6>
2024-02-15 12:13:50 +0530

    Name changes

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,10 +12,10 @@ import { ReactNode } from "react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
-const APP_NAME = "Chatbot UI"
-const APP_DEFAULT_TITLE = "Chatbot UI"
-const APP_TITLE_TEMPLATE = "%s - Chatbot UI"
-const APP_DESCRIPTION = "Chabot UI PWA!"
+const APP_NAME = "inQuestAi Workspace"
+const APP_DEFAULT_TITLE = "inQuestAi Workspace"
+const APP_TITLE_TEMPLATE = "%s - inQuestAi Workspace"
+const APP_DESCRIPTION = "inQuestAi Workspace PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://roganthdeeaqwadumwxm.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJvZ2FudGhkZWVhcXdhZHVtd3htIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzk3NTIwOCwiZXhwIjoyMDIzNTUxMjA4fQ.hzhs_xeeoIquIfHH4ddwu5aP-wxglGQLwHxpTpDgU8A'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
